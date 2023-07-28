f90280c3ffb715886bc9363a45127e9fbdfc136a
Jakub Wojciechowski <jakub@alice.si>
2023-07-28 11:18:53 +0200

    Get Ownable logic from Openzeppelin

--- contracts/RedstoneOracle.sol
+++ contracts/RedstoneOracle.sol
@@ -3,23 +3,10 @@
 pragma solidity 0.8.4;
 
 import "@redstone-finance/evm-connector/contracts/mocks/RedstoneConsumerNumericMock.sol";
+import {Ownable} from '@openzeppelin8/contracts/access/Ownable.sol';
 
-contract RedstoneOracle is RedstoneConsumerNumericMock {
-    address private _owner;
-    address private _pendingOwner;
+contract RedstoneOracle is RedstoneConsumerNumericMock, Ownable {
     mapping (bytes32 => bytes32) priceFeedIdMapping;
-
-    event OwnershipTransferStarted(address indexed previousOwner, address indexed newOwner);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    modifier onlyOwner {
-        require(msg.sender == _owner, "!owner");
-        _;
-    }
-
-    constructor() {
-        _owner = msg.sender;
-    }
 
     /**
      * @dev get price from redstone oracle
@@ -41,40 +28,5 @@ contract RedstoneOracle is RedstoneConsumerNumericMock {
             feedId := mload(add(feedIdStr, 32))
         }
         priceFeedIdMapping[hash] = feedId;
-    }
-
-    /**
-     * @dev Starts the ownership transfer of the contract to a new account. Replaces the pending transfer if there is one.
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address _newOwner) external onlyOwner {
-        require(_newOwner != address(0), "zero address");
-        _pendingOwner = _newOwner;
-        emit OwnershipTransferStarted(owner(), _newOwner);
-    }
-
-    /**
-     * @dev The new owner accepts the ownership transfer.
-     */
-    function acceptOwnership() external {
-        require(pendingOwner() == msg.sender, "caller is not the new owner");
-        delete _pendingOwner;
-        address oldOwner = _owner;
-        _owner = msg.sender;
-        emit OwnershipTransferred(oldOwner, msg.sender);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Returns the address of the pending owner.
-     */
-    function pendingOwner() public view virtual returns (address) {
-        return _pendingOwner;
     }
 }
